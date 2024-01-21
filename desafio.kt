7ebf82db0da737c36ff5d2498159506b893bce99
Thiago Silva <thiago25pro@gmail.com>
2024-01-21 13:31:40 -0300

    Implementa matricula

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,37 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario (val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(usuario: Usuario, nivel: Nivel) {
+        inscritos.add(usuario)
+        println("Matrícula realizada com sucesso para o usuário ${usuario.nome} no curso de ${nome} com nível $nivel.")
+//         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario = Usuario("Thiago")
+    
+    val conteudo = ConteudoEducacional("Kotlin", 1)
+    val conteudo1 = ConteudoEducacional("Java", 2)
+    val conteudo2 = ConteudoEducacional("Node.JS", 3)
+    
+    val conteudos = mutableListOf<ConteudoEducacional>()
+    
+    conteudos.add(conteudo)
+    conteudos.add(conteudo1)
+    conteudos.add(conteudo2)
+    
+    val formacao = Formacao("Dev", conteudos)
+    formacao.matricular(usuario, Nivel.AVANCADO)
+//     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+//     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
